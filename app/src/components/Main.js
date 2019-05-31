825b6a3dc7930647ac99a3530f59f9b50a4792c5
Jacob Bryan <jtb36688@gmail.com>
2019-05-30 23:20:57 -0400

    Created boilerplating for microphone game

--- app/src/components/Main.js
+++ app/src/components/Main.js
@@ -1,48 +1,32 @@
 import React, { Component } from "react";
 import axiosWithAuth from "./utils/axiosWithAuth.js";
 import axios from "axios";
-import { NavLink, Link, withRouter } from "react-router-dom";
+import { Form, FormGroup, Label, Input, Button, Jumbotron } from 'reactstrap';
 
 class Classrooms extends Component {
   constructor() {
     super();
     this.state = {
-      classrooms: []
+      classroomname: "",
+      score: 100
     };
   }
 
-  componentDidMount() {
-    console.log("hihihi");
-
-    axios
-      .get("https://noise-controller.herokuapp.com/api/classrooms")
-      .then(res => {
-        this.setState({ classrooms: res.data });
-        console.log("res", res);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
-
   render() {
-    if (!this.state.classrooms.length)
-      return (
-        <div>
-          <h2>Loading...</h2>
-        </div>
-      );
     return (
-      <div>
-        <div />
-        <div>
-          {this.state.classrooms.map(classroom => (
-            <div key={classroom.id}>
-              <h2>{classroom.classroom_name}</h2>
-              <h3>{classroom.teacher}</h3>
-            </div>
-          ))}
-        </div>
+      <div className="GameWrapper">
+        {this.state.classroomname ?
+          (<div></div>)
+          :
+          (<div className="ClassFormContainer">
+            <Form>
+              <FormGroup>
+                <Label for="exampleEmail">New Classroom Name</Label>
+                <Input className="LoginInput" type="text" id="username" value={this.state.username} onChange={this.handleInputChange} />
+              </FormGroup>
+            </Form>
+          </div>)
+        }
       </div>
     );
   }
